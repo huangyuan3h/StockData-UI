3c411f36e187d36529c6700b80aa8d546b6cc372
huangyuan3h <huangyuan3h@gmail.com>
2021-06-19 22:01:13 +0800

    update the svg file

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import { Header as BaseHeader } from "@mango-solution/mango-ui";
 
+import QuarkIcon from "../../svg/quark.svg";
+
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
   return (
     <BaseHeader>
-      <div className="header-icon">quark quatization</div>
+      <div className="header-icon">
+        <QuarkIcon />
+      </div>
     </BaseHeader>
   );
 };
---
+++ src/pages/search/index.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+import Header from "../../components/Header";
+
+const Index: NextPage = () => {
+  return (
+    <div>
+      <Header />
+      search page
+    </div>
+  );
+};
+
+export default Index;
