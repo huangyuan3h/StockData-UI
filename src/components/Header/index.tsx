4f1560096c05c6f74c40c3af705d808bf76594ac
huangyuan3h <huangyuan3h@gmail.com>
2021-06-21 23:34:20 +0800

    update the ui version

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,38 @@
-import { Header as BaseHeader } from "@mango-solution/mango-ui";
+import React, { useState } from "react";
+import {
+  Header as BaseHeader,
+  Animation,
+  AnimationState,
+  AnimationType,
+} from "@mango-solution/mango-ui";
 
 import QuarkIcon from "../../svg/quark.svg";
 
 interface HeaderProps {}
 
 const Header: React.FC<HeaderProps> = () => {
+  const [IconAnimationState, setIconAnimationState] = useState(
+    AnimationState.initial
+  );
+  const handleMouseOver = () => {
+    setIconAnimationState(AnimationState.active);
+  };
+
+  const handleMouseLeave = () => {
+    setIconAnimationState(AnimationState.inactive);
+  };
   return (
     <BaseHeader>
       <div className="logo">
-        <div className="header-icon">
+        <Animation
+          animationType={AnimationType.spin}
+          animationState={IconAnimationState}
+          onMouseOver={handleMouseOver}
+          onMouseLeave={handleMouseLeave}
+          classes="header-icon"
+        >
           <QuarkIcon />
-        </div>
+        </Animation>
         <div className="header-title">夸克量化</div>
       </div>
     </BaseHeader>
