1af36453be7a91738830aa76f8d202bf73928a6d
huangyuan3h <huangyuan3h@gmail.com>
2021-06-29 23:00:25 +0800

    :sparkles: add menu

--- src/components/Header/HeaderMenu.tsx
+++ src/components/Header/HeaderMenu.tsx
@@ -1,27 +1,61 @@
 import clsx from "clsx";
 import { useRouter } from "next/dist/client/router";
+import { Menu, MenuItem } from "@mango-solution/mango-ui";
+import { useState } from "react";
+
+export interface HeaderMenuItemType {
+  value: string;
+  text: string;
+  path: string;
+}
 
 interface HeaderMenuProps {
   text: string;
   path: string;
   active?: boolean;
+  subMenu?: HeaderMenuItemType[];
 }
 
 const HeaderMenu: React.FC<HeaderMenuProps> = ({
   text,
   path,
   active,
+  subMenu,
 }: HeaderMenuProps) => {
   const router = useRouter();
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
+
+  const hasSubMenu = subMenu && subMenu.length > 0;
 
   const handleClick = () => {
-    router.push(path);
+    if (!hasSubMenu) {
+      router.push(path);
+    }
+    setMenuOpen(true);
+  };
+
+  const handleMenuItemClick = () => {
+    setMenuOpen(false);
+  };
+
+  const renderSubMenu = () => {
+    if (!hasSubMenu) return <></>;
+    return (
+      <Menu hidden={!menuOpen} onClick={handleMenuItemClick}>
+        {subMenu.map(({ value, text }) => (
+          <MenuItem key={value} value={value} label={text} />
+        ))}
+      </Menu>
+    );
   };
 
   return (
-    <div className={clsx("HeaderMenu", { active })} onClick={handleClick}>
-      {text}
-    </div>
+    <>
+      <div className={clsx("HeaderMenu", { active })} onClick={handleClick}>
+        {text}
+      </div>
+      {renderSubMenu()}
+    </>
   );
 };
 
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,31 @@
 import { useRouter } from "next/dist/client/router";
 import React from "react";
 import DistractionFreeHeader from "./DistractionFreeHeader";
-import HeaderMenu from "./HeaderMenu";
+import HeaderMenu, { HeaderMenuItemType } from "./HeaderMenu";
 
 interface HeaderProps {}
 
 enum MenuType {
   homePage = "/",
 }
+
+const quantityMenu: HeaderMenuItemType[] = [
+  {
+    value: "stockData",
+    text: "大盘数据",
+    path: "/stockData",
+  },
+  {
+    value: "methodology",
+    text: "算法",
+    path: "/methodology",
+  },
+  {
+    value: "results",
+    text: "预测结果",
+    path: "/results",
+  },
+];
 
 const Header: React.FC<HeaderProps> = () => {
   const router = useRouter();
@@ -20,7 +38,7 @@ const Header: React.FC<HeaderProps> = () => {
           path="/"
           active={router.pathname === MenuType.homePage}
         />
-        <HeaderMenu text="量化" path="/quantity" />
+        <HeaderMenu text="量化" path="/quantity" subMenu={quantityMenu} />
         <HeaderMenu text="任务" path="/task" />
       </div>
     </DistractionFreeHeader>
